b6e9f149bbc27bbcb8c9e814737b30a0a96ba2ac
Naga Chaitanya Konada <knc@outlook.in>
2020-09-17 11:12:20 +0000

    apollo server side scripting: resolvers

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,23 @@
 require('dotenv').config();
+const { ApolloServer } = require('apollo-server');
+const types = require('./schema');
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+const { createStore } = require('./utils');
+const store = createStore();
+const resolvers = require('./resolvers');
+
+const server = new ApolloServer({
+    typeDefs: types,
+    resolvers,
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({ store })
+    })
+});
+
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+})
+
