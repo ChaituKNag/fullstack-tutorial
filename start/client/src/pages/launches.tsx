837e64b898f1bc5ebe66016918d7d13931623f28
Naga Chaitanya Konada <knc@outlook.in>
2020-09-18 05:23:12 +0000

    launch configs changed.

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,6 +1,8 @@
-import React, { Fragment }  from 'react';
+import React, { Fragment } from 'react';
 import { RouteComponentProps } from '@reach/router';
-import { gql } from '@apollo/client'
+import { gql, useQuery } from '@apollo/client'
+import * as GetLaunchListTypes from './__generated__/GetLaunchList';
+import { Loading, Header, LaunchTile } from '../components';
 
 export const LAUNCH_TILE_DATA = gql`
   fragment LaunchTile on Launch {
@@ -18,10 +20,45 @@ export const LAUNCH_TILE_DATA = gql`
   }
 `;
 
-interface LaunchesProps extends RouteComponentProps {}
+
+export const GET_LAUNCHES = gql`
+    query launchList($after: String) {
+        launches(after: $after) {
+            cursor
+            hasMore
+            launches {
+                id
+                isBooked
+                rocket {
+                    id
+                    name
+                }
+                mission {
+                    name
+                    missionPatch
+                }
+            }
+        }
+    }
+`;
+
+interface LaunchesProps extends RouteComponentProps { }
 
 const Launches: React.FC<LaunchesProps> = () => {
-  return <div />;
+    const { data, loading, error, fetchMore } = useQuery<GetLaunchListTypes.GetLaunchList, GetLaunchListTypes.GetLaunchListVariables>(GET_LAUNCHES);
+
+    if (loading) return <Loading />;
+    if (error) return <p>Error</p>;
+    if (!data) return <p>Not found</p>;
+
+    return (
+        <Fragment>
+            <Header />
+            {data.launches && data.launches.launches && data.launches.launches.map((launch: any) =>
+                <LaunchTile key={launch.id} launch={launch} />
+            )}
+        </Fragment>
+    );
 }
 
 export default Launches;
